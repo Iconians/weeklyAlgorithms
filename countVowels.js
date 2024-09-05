51f07ad35e1607700a8b269040f536e552d21d31
Iconians <claytoncripe@gmail.com>
2024-09-04 17:54:24 -0700

    created the algo files for countVowels and fibonacci

---
+++ countVowels.js
@@ -0,0 +1,4 @@
+// "Prompt: Write a function that takes a string as input and 
+// returns the number of vowels (a, e, i, o, u) in the string.
+// Extension: Modify the function to also return the number of 
+// consonants in the string as well."
---
+++ fibonacci.js
@@ -0,0 +1,4 @@
+// Prompt: Write a recursive function that returns the n-th number 
+// in the Fibonacci sequence using a recursive approach. 
+// Bonus: write a non-recursive, iterative approach.
+// Bonusx2: Memoize this function
