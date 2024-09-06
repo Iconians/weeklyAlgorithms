f43fa9fe10d8b853119f8f26d93747b33f1ad120
Iconians <claytoncripe@gmail.com>
2024-09-05 18:10:23 -0700

    finished countVowels and fibonacci problems

--- countVowels.js
+++ countVowels.js
@@ -2,3 +2,22 @@
 // returns the number of vowels (a, e, i, o, u) in the string.
 // Extension: Modify the function to also return the number of 
 // consonants in the string as well."
+
+const countLetters = (str) => {
+  const transformStr = str.replaceAll(" ", "").toLowerCase()
+  const vowels = new Set(["a", "e", "i", "o", "u"])
+  let vowelCount = 0
+  let consonantsCount = 0
+  if (!/^[a-z]+$/.test(transformStr)) return "only accepts alphabetic characters" 
+
+  for (let letter of transformStr) {
+      if (vowels.has(letter)) {
+        vowelCount++
+      } else {
+        consonantsCount++
+      }
+  }
+  return {vowelCount, consonantsCount}
+}
+
+console.log(countLetters("The rain in spain rains mainly on the plain"))
--- fibonacci.js
+++ fibonacci.js
@@ -2,3 +2,53 @@
 // in the Fibonacci sequence using a recursive approach. 
 // Bonus: write a non-recursive, iterative approach.
 // Bonusx2: Memoize this function
+
+// 1st case recursion
+const fib1 = (num) => {
+  if (num === 0) return 0
+  if (num === 1) return 1
+
+  return fib1(num - 1) + fib1(num - 2)
+}
+
+// console.log(fib1(0));  // Output: 0
+// console.log(fib1(1));  // Output: 1
+// console.log(fib1(6));  // Output: 8
+// console.log(fib1(10)); // Output: 55
+
+// 2nd case iterative 
+const fib2 = (num) => {
+  if (num === 0) return 0
+  if (num === 1) return 1
+  let prev1 = 1
+  let prev2 = 0
+  let result = 0
+
+  for (let i = 2; i <= num; i++) {
+    result = prev1 + prev2
+    prev2 = prev1
+    prev1 = result
+  }
+  return result
+}
+
+console.log(fib2(0));  // Output: 0
+console.log(fib2(1));  // Output: 1
+console.log(fib2(6));  // Output: 8
+console.log(fib2(10)); // Output: 55
+
+// 3rd case memoization
+const fib3 = (num, memo = {}) => {
+  if (num === 0) return 0
+  if (num === 1) return 1
+  if (memo[num]) return memo[num]
+
+  memo[num] = fib3(num - 1, memo) + fib3(num - 2, memo)
+  return memo[num]
+}
+
+// console.log(fib3(0));  // Output: 0
+// console.log(fib3(1));  // Output: 1
+// console.log(fib3(6));  // Output: 8
+// console.log(fib3(10)); // Output: 55
+// console.log(fib3(50)); // Output: 12586269025
