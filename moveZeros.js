6ae7efa26b00c6a6cdf9324de70d2bde011ff3db
Iconians <claytoncripe@gmail.com>
2024-11-05 12:32:18 -0800

    added a streamlined version of moveZeros

--- moveZeros.js
+++ moveZeros.js
@@ -1,15 +1,17 @@
 const moveZeros = (arr) => {
-let zeroArr = []
-let newArr = []
-for (let int of arr) {
-  if (int !== 0) {
-    newArr.push(int)
-  } else {
-    zeroArr.push(int)
+  let zeroArr = [];
+  let newArr = [];
+  for (let int of arr) {
+    if (int !== 0) {
+      newArr.push(int);
+    } else {
+      zeroArr.push(int);
+    }
   }
-  
-}
-return newArr.concat(zeroArr)
-}
+  return newArr.concat(zeroArr);
+};
 
-console.log(moveZeros([0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, ]))
+// array method for same for loop
+// arr.filter(int => int !== 0).concat(arr.filter(int => int === 0))
+
+console.log(moveZeros([0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9]));
